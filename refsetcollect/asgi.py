eac9b6cd2e3b22c888cc4d130354c20890e7dd86
mszg <gergo.msz2000@gmail.com>
2026-03-09 09:55:01 +0100

    directory name change
    changed

--- refsetcollect/asgi.py
+++ refsetcollect/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for samplingtool project.
+ASGI config for refsetcollect project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samplingtool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refsetcollect.settings')
 
 application = get_asgi_application()
--- refsetcollect/wsgi.py
+++ refsetcollect/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for samplingtool project.
+WSGI config for refsetcollect project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samplingtool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'refsetcollect.settings')
 
 application = get_wsgi_application()
